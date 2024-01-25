1b78a86058e7d380340f682cf870989ba9f1b617
W20MC <lebiarka_w20mc@outlook.com>
2024-01-25 21:10:39 +0100

    Update to MusicCloud v2

--- main.py
+++ main.py
@@ -6,23 +6,61 @@ import os
 from discord.ext import commands
 from pytube import YouTube
 
-bot = commands.Bot(command_prefix = "$", intents = discord.Intents.all())
-
+bot = commands.Bot(command_prefix = "=", intents = discord.Intents.all())
 @bot.event
 async def on_ready():
     print("Bot is ready!")
 
 @bot.command()
-async def play(ctx: commands.Context):
+async def play(ctx: commands.Context, url: str):
     user = ctx.message.author
     voice_channel = user.voice.channel
 
+    global voice_client
+    voice_client = ctx.guild.voice_client
+
     channel = None
 
-    if voice_channel != None:
+    if not voice_client in bot.voice_clients:
         channel = voice_channel
+    else:
+        channel = voice_client
 
-        vc = await channel.connect()
-        vc.play(discord.FFmpegPCMAudio("gilld chese.mp3"))
+    if voice_channel != None:
+        yt = YouTube(url)
+        song = yt.streams.filter(only_audio=True).first()
+        FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+        
+        if voice_client in bot.voice_clients:
+            vc = channel
+        else:
+            vc = await channel.connect()
+
+        vc.play(discord.FFmpegPCMAudio(song.url, **FFMPEG_OPTIONS))
+        await ctx.send(f"Playing {yt.title}")
+
+@bot.command()
+async def stop(ctx: commands.Context):
+    user = ctx.message.author
+    voice_channel = user.voice.channel
+
+    global voice_client
+    voice_client = ctx.guild.voice_client
+
+    channel = None
+
+    if not voice_client in bot.voice_clients:
+        channel = voice_channel
+    else:
+        channel = voice_client
+
+    if voice_channel != None:
+        if voice_client in bot.voice_clients:
+            vc = channel
+        else:
+            vc = await channel.connect()
+        
+        vc.stop()
+        await ctx.send(f"Stopping song.")
 
 bot.run(os.getenv("TOKEN"))
